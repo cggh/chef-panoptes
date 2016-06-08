f6725d535e39108a93e4c5e873723d2b3c8be5ce
Ian Wright <ian.wright@well.ox.ac.uk>
2016-06-08 10:11:30 +0100

    Fix typo

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'apt'
-depends 'apache2', '~> v3.2.2'
+depends 'apache2', '~> 3.2.2'
 depends 'git'
 depends 'python'
 depends 'nodejs'
