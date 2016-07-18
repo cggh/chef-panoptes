0452434c53646d7babd70fa5735950da6414a710
Ian Wright <ian.wright@well.ox.ac.uk>
2016-07-18 10:44:43 +0100

    Load samples and variants dataset

--- recipes/sampledata.rb
+++ recipes/sampledata.rb
@@ -6,4 +6,10 @@ remote_directory node["panoptes"]["source_dir"] do
   mode '0770'
   group 'www-data'
   source "sampledata"
+  action :create_if_missing
 end
+
+http_request 'load_sample_data' do
+  url 'http://' + node["panoptes"]["server_name"] + '/api?datatype=custom&respmodule=panoptesserver&respid=fileload_dataset&ScopeStr=all&SkipTableTracks=false&datasetid=Samples_and_Variants'
+end
+
